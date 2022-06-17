0c3b6fda4e509adee6398b7db172e80b5a86e08b
QuackingDucks <107137619+QuackingDucks@users.noreply.github.com>
2022-06-17 10:45:37 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,155 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . f f f . f f f f . f f f . 
+        f f f f f c c c c f f f f f 
+        f f f f b c c c c b f f f f 
+        f f f c 3 c c c c 3 c f f f 
+        . f 3 3 c c c c c c 3 3 f . 
+        . f c c c c c c c c c c f . 
+        . f f c c c c c c c c f f . 
+        . f f f c c c c c c f f f . 
+        . f f f f f f f f f f f f . 
+        . . f f f f f f f f f f . . 
+        . . e f f f f f f f f e . . 
+        . e 4 f f f f f f f f 4 e . 
+        . 4 d f 3 3 3 3 3 3 c d 4 . 
+        . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+        . . . . f f f f f f . . . . 
+        . . . . f f . . f f . . . . 
+        `)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . f f f f f . f f f . 
+        . . . f f c c c c f f f f f 
+        . . f c c c c c c b f f f f 
+        . . f c c c c c c 3 c f f f 
+        . f c c c c c c c c 3 3 f . 
+        . f c c 4 c c c c c f f f . 
+        . f f e 4 4 c c c f f f f . 
+        . f f e 4 4 f b f 4 4 f f . 
+        . . f f d d f 1 4 d 4 f . . 
+        . . . f d d d d 4 f f f . . 
+        . . . f e 4 4 4 e e f . . . 
+        . . . f 3 3 3 e d d 4 . . . 
+        . . . f 3 3 3 e d d e . . . 
+        . . . f 6 6 6 f e e f . . . 
+        . . . . f f f f f f . . . . 
+        . . . . . . f f f . . . . . 
+        `)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . f f f . f f f f f . . . . 
+        f f f f f c c c c f f . . . 
+        f f f f b c c c c c c f . . 
+        f f f c 3 c c c c c c f . . 
+        . f 3 3 c c c c c c c c f . 
+        . f f f c c c c c 4 c c f . 
+        . f f f f c c c 4 4 e f f . 
+        . f f 4 4 f b f 4 4 e f f . 
+        . . f 4 d 4 1 f d d f f . . 
+        . . f f f 4 d d d d f . . . 
+        . . . f e e 4 4 4 e f . . . 
+        . . . 4 d d e 3 3 3 f . . . 
+        . . . e d d e 3 3 3 f . . . 
+        . . . f e e f 6 6 6 f . . . 
+        . . . . f f f f f f . . . . 
+        . . . . . f f f . . . . . . 
+        `)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . f f f . f f f f . f f f . 
+        f f f f f c c c c f f f f f 
+        f f f f b c c c c b f f f f 
+        f f f c 3 c c c c 3 c f f f 
+        . f 3 3 c c c c c c 3 3 f . 
+        . f c c c c 4 4 c c c c f . 
+        . f f c c 4 4 4 4 c c f f . 
+        . f f f b f 4 4 f b f f f . 
+        . f f 4 1 f d d f 1 4 f f . 
+        . . f f d d d d d d f f . . 
+        . . e f e 4 4 4 4 e f e . . 
+        . e 4 f b 3 3 3 3 b f 4 e . 
+        . 4 d f 3 3 3 3 3 3 c d 4 . 
+        . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+        . . . . f f f f f f . . . . 
+        . . . . f f . . f f . . . . 
+        `)
+})
+let mySprite: Sprite = null
+info.setLife(3)
+info.setScore(0)
+mySprite = sprites.create(img`
+    . f f f . f f f f . f f f . 
+    f f f f f c c c c f f f f f 
+    f f f f b c c c c b f f f f 
+    f f f c 3 c c c c 3 c f f f 
+    . f 3 3 c c c c c c 3 3 f . 
+    . f c c c c 4 4 c c c c f . 
+    . f f c c 4 4 4 4 c c f f . 
+    . f f f b f 4 4 f b f f f . 
+    . f f 4 1 f d d f 1 4 f f . 
+    . . f f d d d d d d f f . . 
+    . . e f e 4 4 4 4 e f e . . 
+    . e 4 f b 3 3 3 3 b f 4 e . 
+    . 4 d f 3 3 3 3 3 3 c d 4 . 
+    . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+    . . . . f f f f f f . . . . 
+    . . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+let Monster = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+let myEnemy = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f . . . . . . . . 
+    . . . . . 1 1 f 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 f 1 1 f 1 1 . . . . 
+    . . . . 1 3 1 1 1 1 3 1 . . . . 
+    . . . . 1 1 f 1 1 f f 1 . . . . 
+    . . . . 1 1 f f f f 1 1 . . . . 
+    . . . . . 1 1 1 1 1 1 1 . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . 1 1 1 . . 1 . . 1 1 1 . . 
+    . . . . . . 1 1 1 1 1 1 . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . 1 1 1 1 1 . . . . . . 
+    . . . . 1 . . . . 1 1 . . . . . 
+    `, SpriteKind.Enemy)
+tiles.setCurrentTilemap(tilemap`level1`)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+myEnemy.setPosition(randint(10, 400), randint(10, 400))
+Monster.setPosition(randint(10, 400), randint(10, 400))
+Monster.follow(mySprite, 80)
+myEnemy.follow(mySprite, 60)
+forever(function () {
+    music.playMelody("E B C5 A B G A F ", 120)
+})
